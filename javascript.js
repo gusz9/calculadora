3168e8b13e73e5ddb5be95bc868b8b61adba103c
gusz9 <86850479+gusz9@users.noreply.github.com>
2022-07-02 23:38:19 -0500

    Cambio de variables
    Cambio de nombre de variables

--- javascript.js
+++ javascript.js
@@ -1,117 +1,117 @@
-var frame = document.getElementById("text");
-var a;
-var b;
-var resultado;
+var box_resulted= document.getElementById("text");
+var value1;
+var value2;
+var result;
 var operation;
 
 function number1() {
-  frame.value = frame.value + 1;
+  box_resulted.value = box_resulted.value + 1;
 }
 
 function number2() {
-  frame.value = frame.value + 2;
+  box_result.value = box_result.value + 2;
 }
 
 function number3() {
-  frame.value = frame.value + 3;
+  box_result.value = box_result.value + 3;
 }
 
 function number4() {
-  frame.value = frame.value + 4;
+  box_result.value = box_result.value + 4;
 }
 
 function number5() {
-  frame.value = frame.value + 5;
+  box_result.value = box_result.value + 5;
 }
 
 function number6() {
-  frame.value = frame.value + 6;
+  box_result.value = box_result.value + 6;
 }
 
 function number7() {
-  frame.value = frame.value + 7;
+  box_result.value = box_result.value + 7;
 }
 
 function number8() {
-  frame.value = frame.value + 8;
+  box_result.value = box_result.value + 8;
 }
 
 function number9() {
-  frame.value = frame.value + 9;
+  frame.value = box_result.value + 9;
 }
 
 function number0() {
-  frame.value = frame.value + 0;
+  box_result.value = box_result.value + 0;
 }
 
 function punto() {
-  frame.value = frame.value + ".";
+  box_result.value = box_result.value + ".";
 }
 
 function buttonAdd() {
-  if(a != undefined) {
+  if(value1 != undefined) {
     operation = "+";
-    frame.value = "";
+    box-result.value = "";
   }else {
-    a = Number(frame.value);
+    value1 = Number(box_result.value);
     operation = "+";
-    frame.value = "";
+    box_result.value = "";
   }
 }
 
 function buttonSub() {
-  if(a != undefined) {
+  if(value1 != undefined) {
     operation = "-";
-    frame.value = "";
+    box_result.value = "";
   }else {
-    a = Number(frame.value);
+    value1 = Number(box_result.value);
     operation = "-";
-    frame.value = "";
+    box_result.value = "";
   }
 }
 
 function buttonMul() {
-  if(a != undefined) {
+  if(value1 != undefined) {
     operation = "x";
-    frame.value = "";
+    box_result.value = "";
   }else {
-    a = Number(frame.value);
+    value1 = Number(box_result.value);
     operation = "x";
-    frame.value = "";
+    box_result.value = "";
   }
 }
 
 function buttonDiv() {
-  if(a != undefined) {
+  if(value1 != undefined) {
     operation = "/";
-    frame.value = "";
+    box_result.value = "";
   }else {
-    a = Number(frame.value);
+    value1 = Number(box_result.value);
     operation = "/";
-    frame.value = "";
+    box_result.value = "";
   }
 }
 
 function buttonClear() {
-  frame.value = "";
+  box_result.value = "";
 }
 
 function buttonRes() {
   if (operation === "+") {
-    b = Number(frame.value);
-    frame.value = a + b;
-    a = undefined;
+    value2 = Number(box_result.value);
+    box_result.value = value1 + value2;
+    value1 = undefined;
   } else if (operation === "-") {
-    b = Number(frame.value);
-    frame.value = a - b;
-    a = undefined;
+    value2 = Number(box-result.value);
+    box_result.value = value1 - value2;
+    value1 = undefined;
   } else if (operation === "x") {
-    b = Number(frame.value);
-    frame.value = a * b;
-    a = undefined;
+    value2 = Number(box_result.value);
+    box_result.value = value1 * value2;
+    value1 = undefined;
   } else if (operation === "/") {
-    b = Number(frame.value);
-    frame.value = a / b;
-    a = undefined;
+    value2 = Number(box_result.value);
+    box_result.value = value1 / value2;
+    value1 = undefined;
   }
 }
